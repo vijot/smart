64b660473813846cf2021c88f54027cfd8f71cd6
vijot <smartjothy@gmail.com>
2016-07-08 13:46:33 +0530

    first commit experiment-addcycle

---
+++ add2website/Home.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="VB" MasterPageFile="~/MasterPage.master" AutoEventWireup="false" CodeFile="Home.aspx.vb" Inherits="_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+    <div style="height: 275px">
+        height:800</div>
+    <p style="background-color: #FFFFFF; height: 414px;">
+        <br />
+    </p>
+    </asp:Content>
+
---
+++ add2website/MasterPage.master.vb
@@ -0,0 +1,44 @@
+﻿Imports System
+Imports System.Web
+Imports System.Web.UI
+Imports System.IO
+
+
+
+
+Partial Class MasterPage
+    Inherits System.Web.UI.MasterPage
+   Public Dim i As Integer = 0
+
+    Public Sub Page_Init(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Init
+        i = 1
+
+
+    End Sub
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+
+    End Sub
+End Class
+
